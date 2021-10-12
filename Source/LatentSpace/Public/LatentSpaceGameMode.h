a469c1ac60d63ce4370ba6df2cab94339952b27a
Bikecicle <griffinpage9@gmail.com>
2021-10-12 11:44:58 -0400

    Controller adjusted for gravity

---
+++ Source/LatentSpace/Public/LatentSpaceGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LatentSpaceGameMode.generated.h"
+
+UCLASS(config=Game)
+class ALatentSpaceGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALatentSpaceGameMode();
+};
+
+
+
